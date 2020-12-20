e14749b579d3a73051dad7af08de81d63446883a
Qvl22 <sashca22@gmail.com>
2020-12-20 18:43:36 +0200

    Add project

---
+++ GIT/Oleksandr_kovalenko.feature
@@ -0,0 +1,52 @@
+﻿Feature: Testing Epam web-site
+
+
+Background: 
+    Given I opened Epam web-site
+
+
+Scenario: Select Region
+	Given Click "Global (EN)"	
+	When I select "Ukraine(English)"
+	Then I should be redirected to Ukrainian region English web-page
+
+Scenario: Read about company
+    Given I am on the ane page
+	When I click "Company" button
+	Then I should see all info about EPAM.
+
+Scenario: Look at vacancies
+    Given I am on the any page.
+	When I click "Vacancies" button
+	Then I should see menu for finding vacancies
+
+Scenario: Finding McDonald`s cashier vacancies
+	Given I`m on "Vacancies" page
+	When I printed "McDonalds" to search string
+	Then I should see "McDonald`s" vacancies
+	And vacancy for cashier too
+
+Scenario: Looking at Privacy Policy
+	Given I am on the any page
+	And I scroll down the page 
+	When I clicked at Privacy Policy
+	Then Site should show me all the needed information
+	
+Scenario: Using a Search
+	Given I am on the main page
+	* I click on the searching button
+	* Serch menu drops down
+	* I type in "Blockchain" in search field
+	When I click "FIND" button
+	Then I should see everything about Blockchain at EPAM
+
+Scenario: Adaptive structure
+	Given I am on the contact page 
+	When I click "CONTACT US" button
+	Then browser should onload new page with a contact form
+
+	Scenario: Info about EPAM and Healthcare
+	Given I am on the any page
+	And Put my mouse on the "Our Work"
+	When I clicked on Healthcare
+	Then I should see all the info about Healthcare
---
+++ GIT/Selenium.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Interactions;
+
+
+namespace Guru99Demo
+{
+    class Guru99Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("D:\\testing\\TAF");
+            driver.Url = "http://www.epam.com";
+            driver.Manage().Window.Maximize();
+            IWebElement el = driver.FindElement(By.XPath("/ html / body / div[1] / div[1] / div / div / div[2] / button"));
+            el.Click();
+        }
+
+        [Test]
+        public void test1()
+        {
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/header/div/ul/li[2]/div/button"));
+            element.Click();
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/header/div/ul/li[2]/div/nav/ul/li[8]/a"));
+            while (element.Displayed == false) { }
+            element.Click();
+
+        }
+
+        [Test]
+
+        public void test2()
+        {
+            driver.Url = "https://careers.epam.ua/company";
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[2]/div/div/header/div/nav/ul/li[1]/span/a"));
+            element.Click();
+            
+        }
+        [Test]
+
+        public void test3()
+        {
+            driver.Url = "https://careers.epam.ua/company";
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[2]/div/div/header/div/nav/ul/li[2]/span/a"));
+            element.Click();
+        }
+
+        [Test]
+        public void test4()
+        {
+            driver.Url = "https://careers.epam.ua/vacancies";
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/main/div[1]/div[2]/section/div/div[2]/div/form/div[1]/input"));
+            element.SendKeys("McDonald`s");
+            element = driver.FindElement(By.XPath("/html/body/div[2]/main/div[1]/div[2]/section/div/div[2]/div/form/button"));
+            element.Click();
+        }
+        [Test]
+        public void test5()
+        {
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/div[3]/div[1]/footer/div/div[2]/ul[1]/li[3]/a"));
+            element.Click();
+        }
+        [Test]
+        public void test6()
+        {
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/header/div/ul/li[3]/div/button"));
+            element.Click();
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/header/div/ul/li[3]/div/div/form/div/input"));
+            element.SendKeys("Blockchain");
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/header/div/ul/li[3]/div/div/form/button"));
+            element.Click();
+
+        }
+        [Test]
+        public void test7()
+        {
+            driver.Url = "https://careers.epam.ua/contact";
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/main/div[1]/div[6]/section/div/div[3]/div/a/span[1]"));
+
+        }
+        [Test]
+        public void test8()
+        {
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/header/div/nav/ul/li[3]/span[1]/a"));
+            Actions action = new Actions(driver);
+            action.MoveToElement(element).Build().Perform();
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/header/div/nav/ul/li[3]/div/ul[2]/li[4]/a"));
+            element.Click();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+    
+}
